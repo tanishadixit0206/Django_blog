a50163a2abacd2d573d5dbddb8a5a88883303bf8
dixittanisha02 <dixittanisha02@gmail.com>
2024-03-20 23:59:49 +0530

    bfix static path error

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR /"blog_project"/'static'
 
 # Application definition
 
