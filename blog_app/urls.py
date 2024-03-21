8d338c0344910aa3a75c10415461d84e1cc872e8
dixittanisha02 <dixittanisha02@gmail.com>
2024-03-21 06:15:34 +0530

    bfix app_name error blog_app

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name='blog_app'
+# app_name='blog_app'
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
